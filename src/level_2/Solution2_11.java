dd58fbec5f1bd202924c3719cc1867ec4510284b
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:36:35 +0300

    Закомментируй максимальное количество строк, чтобы на экран вывелось число 19
    Требования:
    1. Программа должна выводить на экран число 19.
    2. Нельзя изменять строки с объявлением переменных.
    3. Нельзя изменять строку отвечающую за вывод в консоль.
    4. Нужно закомментировать максимальное количество необходимых строк и не менять остальные.

---
+++ src/level_2/Solution2_11.java
@@ -0,0 +1,24 @@
+package level_2;
+
+public class Solution2_11 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 0;
+
+        y = y + x;
+
+        x = x * 2;
+        y = y + x;
+
+        x = x * 2;
+        //y = y + x;
+
+        x = x * 2;
+        //y = y + x;
+
+        x = x * 2;
+        y = y + x;
+
+        System.out.println(y);
+    }
+}
