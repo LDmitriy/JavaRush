6360a091ae614c068f5288790c6bc940a1f5809f
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:01:51 +0300

    Напиши программу, которая вызывает метод sum с параметрами 2 и 2.
    Требования:
    1. Программа должна выводить сумму 2 и 2.
    2. Метод main должен вызывать метод sum с параметрами 2 и 2.
    3. Метод main не должен вызывать команду вывода текста на экран.
    4. Метод sum изменять нельзя.

---
+++ src/level_1/Solution1_19.java
@@ -0,0 +1,12 @@
+package level_1;
+
+public class Solution1_19 {
+    public static void main(String[] args) {
+        sum(2,2);//напишите тут ваш код
+    }
+
+    public static void sum(int a, int b) {
+        int c = a + b;
+        System.out.print(c);
+    }
+}
