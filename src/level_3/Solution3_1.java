dfbf95b53a5fb8e597d52422afa7a2b77b61a1f5
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 21:30:06 +0300

    Немедленно в печать Добавь метод public static void printString(String s), в нем напиши код, который будет выводить переданную строку на экран.
    Требования:
    1. Добавь метод printString, у которого аргумент имеет тип String.
    2. Метод printString должен быть void.
    3. Метод printString должен быть static.
    4. Метод printString должен быть public.
    5. Метод printString должен выводить переданный текст на экран.
    6. Программа должна вывести "Hello Amigo!".

---
+++ src/level_3/Solution3_1.java
@@ -0,0 +1,11 @@
+package level_3;
+
+public class Solution3_1 {
+    public static void printString(String s){
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        printString("Hello Amigo!");
+    }
+}
