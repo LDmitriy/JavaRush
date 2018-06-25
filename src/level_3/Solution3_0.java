312f81844b961f0d0128caf8831aac4c8a50311f
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:51:13 +0300

    Реализуй метод public static void div(int a, int b). Метод должен делить первое число на второе, и выводить на экран результат деления а на b. На экран должно быть выведено целое число.
    Требования:
    1. Функция div должна делить а на b.
    2. Функция div должна выводить результат деления на экран.
    3. Функция main должна вызывать функцию div 3 раза.
    4. Функция main не должна вызывать команду вывода текста на экран.
    5. Программа должна вывести три числа: 2 1 0. Каждое число в отдельной строке.

---
+++ src/level_3/Solution3_0.java
@@ -0,0 +1,13 @@
+package level_3;
+
+public class Solution3_0 {
+    public static void main(String[] args) {
+        div(6, 3);
+        div(10, 6);
+        div(2, 4);
+    }
+
+    public static void div(int a, int b) {
+        System.out.println((int)(a/b));
+    }
+}
