c4fe184a65a738ac084a560332d9ccb79fea5379
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:47:38 +0300

    Реализуй метод print3. Метод должен вывести на экран переданную строку 3 раза. Каждый раз с новой строки.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать функцию System.out.println или System.out.print.
    3. Метод print3 должен выводить текст на экран.
    4. Метод main должен вызвать метод print3 только один раз.
    5. Метод print3 должен выводить на экран строку 3 раза. Каждый раз с новой строки.

---
+++ src/level_2/Solution2_16.java
@@ -0,0 +1,14 @@
+package level_2;
+
+public class Solution2_16 {
+    public static void print3(String s) {
+        for(int i = 0; i< 3; i++){
+            System.out.println(s);
+        }//напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+}
