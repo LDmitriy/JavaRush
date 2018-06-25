bd70504351bac1b44589526275dd943a6d63842a
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 19:29:48 +0300

    Дана переменная number. Напиши программу, которая выводит на экран квадрат этой переменной (number * number).
    Требования:
    1. Переменную number не изменяй, можешь менять только ее значение.
    2. Программа должна выводить текст.
    3. Выводимый текст должен быть числом.
    4. Выводимый текст должен быть квадратом переменной number.

---
+++ src/level_1/Solution1_4.java
@@ -0,0 +1,9 @@
+package level_1;
+
+public class Solution1_4 {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        System.out.println(number*number);
+    }
+}
