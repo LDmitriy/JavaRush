d915a94adcac681985481d0182bc9be036cce25b
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 23:59:41 +0300

    Головоломка со скобками Расставить правильно (по другому) скобки, чтобы на экран вывелось число 850
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. Метод main должен выводить на экран число 850.
    3. Метод main должен содержать только один вызов System.out.println.
    4. Последовательность цифр и арифметических операций изменять нельзя.

---
+++ src/level_3/Solution3_5.java
@@ -0,0 +1,7 @@
+package level_3;
+
+public class Solution3_5 {
+    public static void main(String[] args) {
+        System.out.println(1 + 2 * (3 + 4 * 5 + 6 * 7 + 8 * 9 + 10 * 11 + 12 * 13 + 14) + 15);
+    }
+}
