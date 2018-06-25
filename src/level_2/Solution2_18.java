ea8cb83840fd535d934e6e71d503e6645e82bf4d
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:49:45 +0300

    - Амиго, ты знал, что сила тяжести на Луне составляет примерно 17% от силы тяжести на Земле? - Нет. - Вот и я не знал. А теперь этой информацией нужно пользоваться часто, и, чтобы не считать каждый раз, реализуй метод getWeight(int), который принимает вес тела (в Ньютонах) на Земле, и возвращает, сколько это тело будет весить на Луне (в Ньютонах). Тип возвращаемого значения - double.
    Пример:
    Метод getWeight вызывается с параметром 888.
    
    Пример вывода:
    150.96
    
    Требования:
    1. Метод getWeight(int) должен быть публичным и статическим.
    2. Метод getWeight должен возвращать значение типа double.
    3. Метод getWeight не должен ничего выводить на экран.
    4. Метод getWeight должен правильно переводить вес тела в Ньютонах на Земле в вес этого же тела на Луне, и возвращать это значение.

---
+++ src/level_2/Solution2_18.java
@@ -0,0 +1,12 @@
+package level_2;
+
+public class Solution2_18 {
+    public static void main(String[] args) {
+        System.out.println(getWeight(888));
+    }
+
+    public static double getWeight(int weightEarth) {
+        double m = weightEarth * 0.17;
+        return m;//напишите тут ваш код
+    }
+}
