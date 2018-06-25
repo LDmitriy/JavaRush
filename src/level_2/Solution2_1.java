f89ae3d777bcf693140194c526edacddd39f1d3c
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:06:25 +0300

    В методе print выведи на экран переданную строку 4 раза. Каждый раз с новой строки.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать System.out.println или System.out.print.
    3. Метод print должен выводить текст на экран.
    4. Метод main должен вызвать метод print класса Solution ровно два раза.
    5. Метод print должен выводить на экран строку 4 раза. Каждый раз с новой строки.

---
+++ src/level_2/Solution2_1.java
@@ -0,0 +1,14 @@
+package level_2;
+
+public class Solution2_1 {
+    public static void main(String[] args) {
+        print("Java easy to learn!");
+        print("Java opens many opportunities!");
+    }
+
+    public static void print(String s) {
+        for(int i =0;i<4;i++){
+            System.out.println(s);
+        }
+    }
+}
