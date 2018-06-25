8a504e1732dbd3a4a70c70edfdd219e6ec204c18
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:04:29 +0300

    В методе main напиши код, чтобы в результате выполнения программы было выведено твое любимое стихотворение. В стихотворении должно быть не меньше, чем 4 строчки. Не убирай вывод строчки "Мое любимое стихотворение:".
    Требования:
    1. Программа должна выводить текст.
    2. Перед стихом должна быть выведена строка "Мое любимое стихотворение:".
    3. Выводимый текст должен состоять не меньше, чем из пяти строк (пустые строки - это тоже строки).
    4. В выводимом тексте должно быть не менее, чем 50 символов.

---
+++ src/level_1/Solution1_21.java
@@ -0,0 +1,16 @@
+package level_1;
+
+public class Solution1_21 {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("Another head hangs lowly");
+        System.out.println("Child is slowly taken");
+        System.out.println("And the violence caused such silence");
+        System.out.println("Who are we mistaking?");
+        System.out.println("But, you see it's not me");
+        System.out.println("It's not my family");
+        System.out.println("In your head, in your head");
+        System.out.println("They are fighting");
+
+    }
+}
