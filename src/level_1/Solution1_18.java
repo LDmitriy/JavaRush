71ed7c3f405b6621de95b4bc67da79d24987573e
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 19:58:31 +0300

    Напиши программу, которая выводит на экран квадрат числа 5.
    Требования:
    1. Программа должна выводить текст.
    2. Метод main должен вызывать метод sqr с параметром 5.
    3. Метод main должен выводить результат метода sqr.
    4. Метод sqr изменять нельзя.
    5. Выводимый текст должен соответствовать заданию.

---
+++ src/level_1/Solution1_18.java
@@ -0,0 +1,12 @@
+package level_1;
+
+public class Solution1_18 {
+    public static void main(String[] args) {
+        int x = sqr(5);
+        System.out.println(x);
+    }
+
+    public static int sqr(int a) {
+        return a * a;
+    }
+}
