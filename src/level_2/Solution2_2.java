6f8bfd126dc294ffef61086e6f31e29090829007
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:08:57 +0300

    В классе Person объяви следующие переменные: name типа String, age типа int, weight типа int, money типа int. В методе main создай объект Person, занеси его ссылку в переменную person. Подсказка: для создания объекта Person и занесения его ссылки в переменную person используй конструкцию: ТипПеременной имяПеременной = new ТипСоздаваемогоОбъекта();
    Требования:
    1. В классе Person объяви переменную name типа String.
    2. В классе Person объяви переменную age типа int.
    3. В классе Person объяви переменную weight типа int.
    4. В классе Person объяви переменную money типа int.
    5. В методе main создай объект Person и сразу присвой ссылку на него переменной person.
    6. Должно быть объявлено 5 переменных.

---
+++ src/level_2/Solution2_2.java
@@ -0,0 +1,12 @@
+package level_2;
+
+public class Solution2_2 {
+    public static void main(String[] args) {
+        Person person = new Person();//напишите тут ваш код
+    }
+
+    public static class Person {
+        String name;
+        int age,weight,money;//напишите тут ваш код
+    }
+}
