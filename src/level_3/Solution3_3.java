459fb4bf786b3f670362552e291fc6f7bb93e81e
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 21:43:12 +0300

    Задача на проценты Реализуй метод addTenPercent, который увеличивает переданное целое число на 10%. Исправь ошибку в объявлении метода.
    Подсказка:
    подумай, какие у этого метода входные и выходные данные.
    
    Требования:
    1. Тип возвращаемого значения у метода addTenPercent должен соответствовать заданию.
    2. Метод addTenPercent должен увеличивать переданное число на 10% процентов.
    3. Метод main должен вызывать метод addTenPercent.
    4. Метод main должен выводить результаты вызова на экран.
    5. Метод addTenPercent не должен ничего выводить на экран.

---
+++ src/level_3/Solution3_3.java
@@ -0,0 +1,12 @@
+package level_3;
+
+public class Solution3_3 {
+    public static double addTenPercent(int i) {
+        double x = i * 1.1;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addTenPercent(9));
+    }
+}
