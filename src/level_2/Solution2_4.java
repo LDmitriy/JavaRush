d725d1da1518883d2e7fd0196eb8a56d890646c9
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:11:03 +0300

    В методе public static void salary(int a) увеличь зарплату на 100 и выведи на экран надпись: "Твоя зарплата составляет: a долларов в месяц." Где a - это зарплата, которую увеличили на 100.
    Пример вывода на экран для а = 700:
    Твоя зарплата составляет: 800 долларов в месяц.
    
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать System.out.println или System.out.print.
    3. Метод salary не должен возвращать результат.
    4. Метод main должен вызвать метод salary только один раз.
    5. Метод salary должен увеличить переданное число на 100 и вывести на экран надпись согласно шаблону.

---
+++ src/level_2/Solution2_4.java
@@ -0,0 +1,12 @@
+package level_2;
+
+public class Solution2_4 {
+    public static void main(String[] args) {
+        salary(700);
+    }
+
+    public static void salary(int a) {
+        a+=100;
+        System.out.println("Твоя зарплата составляет: "+ a +" долларов в месяц.");//напишите тут ваш код
+    }
+}
