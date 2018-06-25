100668895e2e6c708c9b6565a1312bac54ac2623
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:03:27 +0300

    Добавь вывод на экран последнюю строку первого куплета известного в ХХ веке произведения "Парус" М.Ю. Лермонтова.
    Требования:
    1. Программа должна выводить текст.
    2. Текст должен начинаться с "Белеет парус одинокий".
    3. Текст должен состоять из 4 строк (пустые строки - это тоже строки).
    4. Выводимый текст должен соответствовать заданию.

---
+++ src/level_1/Solution1_20.java
@@ -0,0 +1,10 @@
+package level_1;
+
+public class Solution1_20 {
+    public static void main(String[] args) {
+        System.out.println("Белеет парус одинокий");
+        System.out.println("В тумане моря голубом!...");
+        System.out.println("Что ищет он в стране далекой?");
+        System.out.println("Что кинул он в краю родном?...");
+    }
+}
