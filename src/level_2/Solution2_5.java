f4a2ebde82a29e0f18430a31fba1b4be96e0a4a3
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:28:32 +0300

    В методе main объяви две переменные a и b типа int. Присвой им любые значения. Выведи на экран их сумму и произведение. Каждое значение выводить с новой строки.
    Пример вывода на экран для а = 5, b = 7:
    12
    35
    
    Требования:
    1. Программа должна выводить текст на экран.
    2. В программе должны быть только две переменные a и b типа int.
    3. Переменным сразу должны быть присвоены значения.
    4. Метод main должен выводить на экран сумму переменных a и b.
    5. Метод main должен выводить на экран произведение переменных a и b.
    6. Каждое значение должно быть выведено с новой строки

---
+++ src/level_2/Solution2_5.java
@@ -0,0 +1,9 @@
+package level_2;
+
+public class Solution2_5 {
+    public static void main(String[] args) {
+        int a=5,b=4;
+        System.out.println(a+b);
+        System.out.println(a*b);//напишите тут ваш код
+    }
+}
