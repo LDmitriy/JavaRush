959d8029e25438a9d0cd31235877625a63d194d9
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:33:21 +0300

    Создать объект типа Cat 2 раза. Сохрани каждый экземпляр в свою переменную. Имена переменных должны быть разные.
    Требования:
    1. Программа не должна выводить текст на экран.
    2. В методе main должно быть только две переменные типа Cat.
    3. Переменным сразу должны быть присвоены значения.
    4. В классе Cat не должно быть переменных.
    5. В классе Cat не должно быть методов.

---
+++ src/level_2/Solution2_8.java
@@ -0,0 +1,12 @@
+package level_2;
+
+public class Solution2_8 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Cat cat2 = new Cat();//напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
