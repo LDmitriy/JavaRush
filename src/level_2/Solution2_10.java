ba88cd7a5d628e39c6ef1dea403740ad4b00d476
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:35:20 +0300

    Напиши программу, которая выводит на экран надпись: "Будешь плохо кодить, придет Java и съест твою память".
    Требования:
    1. Программа должна выводить текст на экран.
    2. Текст должен начинаться с "Будешь".
    3. Текст должен заканчиваться на "память".
    4. Текст должен состоять из 52 символов, включая пробелы и знаки препинания.
    5. Выводимый текст должен соответствовать заданию.

---
+++ src/level_2/Solution2_10.java
@@ -0,0 +1,9 @@
+package level_2;
+
+public class Solution2_10 {
+    public static void main(String[] args) {
+        System.out.println("Будешь плохо кодить, придет Java и съест твою память");
+
+//напишите тут ваш код
+    }
+}
