c1ace251e4033e5026a649bbbe9dfb19846e0b94
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 19:47:34 +0300

    Внеси изменения в программу, чтобы переменная age имела значение 15.
    Требования:
    1. Должно быть объявлено 4 переменных.
    2. Тип переменной age должен быть int.
    3. Всем переменным должны присваиваться значение сразу после создания.
    4. Значение, которое присваивается переменной age, должно быть равно 15.

---
+++ src/level_1/Solution1_10.java
@@ -0,0 +1,10 @@
+package level_1;
+
+public class Solution1_10 {
+    public static void main(String[] args) {
+        String name = "Amigo";
+        int age = 15;
+        int money = 500;
+        int weight = 70;
+    }
+}
