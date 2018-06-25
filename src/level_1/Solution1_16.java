d5aae027d1e7d5dc2f1a9b277ba930e58533a17f
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 19:55:39 +0300

    Объяви недостающие переменные и закомментируй те, которые нигде не используются.
    Требования:
    1. Программа должна выводить на экран текст.
    2. Должно быть объявлено 4 переменные.
    3. Нужно закомментировать неиспользуемые переменные.
    4. Вывод программы не должен измениться.

---
+++ src/level_1/Solution1_16.java
@@ -0,0 +1,14 @@
+package level_1;
+
+public class Solution1_16 {
+    public static void main(String[] args) {
+        //String s = "15";
+        int a = 5;
+        //int z = 18;
+        int d = 18;
+        int c = a + d;
+        String b = " better than ";
+
+        System.out.println(a + b + c);
+    }
+}
