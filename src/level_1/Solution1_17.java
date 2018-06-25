e67867de5b6bee9c8440fbba9bcca76abc2e2c34
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 19:56:40 +0300

    Напиши программу, которая выводит на экран надпись: "Хочешь - решай задачи, не хочешь - решай нехотя" 16 раз.
    Требования:
    1. Программа должна выводить текст.
    2. Текст должен начинаться с "Хочешь".
    3. Текст должен заканчиваться на "нехотя".
    4. Текст должен состоять из 16 строк.
    5. Выводимый текст должен соответствовать заданию.

---
+++ src/level_1/Solution1_17.java
@@ -0,0 +1,12 @@
+package level_1;
+
+public class Solution1_17 {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<16){
+            i++;
+            System.out.println("Хочешь - решай задачи, не хочешь - решай нехотя");
+
+        }
+    }
+}
