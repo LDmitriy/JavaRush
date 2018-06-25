d91c161464c8c11826ef1876755af98beb0a6e4c
LDmitriy <uks.loboyko@gmail.com>
2018-06-25 20:30:21 +0300

    Выведи на экран длину окружности, рассчитанную по формуле: L = 2 * Pi * radius. Результат - дробное число (тип double). В качестве значения Pi используй значение 3.14.
    Требования:
    1. В методе printCircleLength нужно вывести длину окружности, рассчитанной по формуле: 2 * Pi * radius.
    2. Метод main должен вызывать метод printCircleLength с параметром 5.
    3. Метод main не должен вызывать команду вывода текста на экран.
    4. Программа должна выводить длину окружности с радиусом 5.

---
+++ src/level_2/Solution2_6.java
@@ -0,0 +1,13 @@
+package level_2;
+
+public class Solution2_6 {
+    public static void main(String[] args) {
+        printCircleLength(5);
+    }
+
+    public static void printCircleLength(int radius) {
+        //double Pi = 3.14;
+        //double L = 2*Pi*radius;
+        System.out.println(radius*2*3.14);//напишите тут ваш код
+    }
+}
