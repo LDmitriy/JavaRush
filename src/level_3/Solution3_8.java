a3da4449c331f4fc4fbaeca534ff960fdb2f56a5
LDmitriy <uks.loboyko@gmail.com>
2018-06-27 11:30:58 +0300

    Сумма 10 чисел Вывести на экран сумму чисел от 1 до 5 построчно (должно быть 5 строк): 1 1+2=3 1+2+3=6 ...
    Пример вывода:
    1
    3
    6
    ...
    
    Требования:
    1. Программа должна выводить текст.
    2. Выведенный текст должен содержать 5 строк.
    3. Число в каждой новой строке должно быть больше предыдущего.
    4. Выводимый текст должен соответствовать заданию.

---
+++ src/level_3/Solution3_8.java
@@ -0,0 +1,11 @@
+package level_3;
+
+public class Solution3_8 {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+        for (int i = 2; i <6; i++){
+            System.out.println(x = x+i);
+        }
+    }
+}
