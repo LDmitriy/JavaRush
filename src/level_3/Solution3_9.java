a62def880d23779fd281d1711da32929bdacc695
LDmitriy <uks.loboyko@gmail.com>
2018-06-27 11:35:11 +0300

    Печатаем строки Реализуй метод public static void writeToConsole(String s), который добавляет к началу строки выражение "printing: " и выводит измененную строку на экран.
    Пример вывода для "Hello world!":
    printing: Hello world!
    
    Требования:
    1. Добавь метод writeToConsole, у которого аргумент имеет тип String.
    2. Метод writeToConsole должен быть void.
    3. Метод writeToConsole должен быть static.
    4. Метод writeToConsole должен быть public.
    5. Метод writeToConsole должен добавлять к началу строки "printing: " и выводить полученный результат на экран.
    6. Программа должна вывести "printing: Hello world!".

---
+++ src/level_3/Solution3_9.java
@@ -0,0 +1,11 @@
+package level_3;
+
+public class Solution3_9 {
+    public static void main(String[] args) {
+        writeToConsole("Hello world!");
+    }
+
+    public static void writeToConsole(String s) {
+        System.out.println("printing: " + s);
+    }
+}
