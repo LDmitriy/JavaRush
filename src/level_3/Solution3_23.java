19dd853f6cbea7e09b240acf1d98eeef742d3e48
LDmitriy <uks.loboyko@gmail.com>
2018-07-01 17:55:05 +0300

    Меркантильные намерения Вывести на экран десять раз надпись "Я хочу большую зарплату, и для этого изучаю Java"
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. Программа должна выводить текст.
    3. Текст должен начинаться с "Я хочу".
    4. Текст должен заканчиваться на "изучаю Java".
    5. Текст должен состоять из 10 строк.
    6. Выводимый текст должен соответствовать заданию.

---
+++ src/level_3/Solution3_23.java
@@ -0,0 +1,9 @@
+package level_3;
+
+public class Solution3_23 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        }
+    }
+}
