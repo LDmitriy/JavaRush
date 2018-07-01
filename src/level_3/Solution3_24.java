998f5f75ca927ea045d43cbdb692fe1692270496
LDmitriy <uks.loboyko@gmail.com>
2018-07-01 17:58:47 +0300

    Финансовые ожидания Ввести с клавиатуры число n. Вывести на экран надпись "Я буду зарабатывать $n в час".
    Пример:
    Я буду зарабатывать $50 в час
    
    Требования:
    1. Программа должна выводить текст.
    2. Программа должна считывать данные с клавиатуры.
    3. Выведенный текст должен содержать введенное число n.
    4. Выведенный тест должен полностью соответствовать заданию.

---
+++ src/level_3/Solution3_24.java
@@ -0,0 +1,12 @@
+package level_3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution3_24 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String n = reader.readLine();
+        System.out.println("Я буду зарабатывать $"+n+" в час");
+    }
+}
