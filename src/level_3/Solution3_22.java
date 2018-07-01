44483dd7d07804bc1bf1b467e0927a49ba787915
LDmitriy <uks.loboyko@gmail.com>
2018-07-01 17:43:19 +0300

    Не думать о секундах… Напиши код, который считает сколько секунд прошло с 15:00, если на часах 15:30. Выведи результат на экран.
    Требования:
    1. Программа должна выводить текст.
    2. Выведенный текст должен быть целым положительным числом.
    3. Выведенное число должно быть кратно 60.
    4. Выводимое число должно соответствовать заданию.

---
+++ src/level_3/Solution3_22.java
@@ -0,0 +1,8 @@
+package level_3;
+
+public class Solution3_22 {
+    public static void main(String[] args) {
+        int secondsAfter15 = 30*60;
+        System.out.println(secondsAfter15);
+    }
+}
