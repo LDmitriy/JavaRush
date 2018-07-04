a757f295d196fa32ee5b6e3777bbae834430d5c7
LDmitriy <uks.loboyko@gmail.com>
2018-07-04 17:43:18 +0300

    Учет котов Напиши код в методе addNewCat, чтобы при его вызове количество котов увеличивалось на 1. За количество котов отвечает переменная catsCount.
    Требования:
    1. Класс Cat должен содержать только одну переменную catsCount.
    2. Переменная catsCount должна быть статической, иметь модификатор доступа private, тип int и проинициализирована нулем.
    3. Класс Cat должен содержать два метода addNewCat и main.
    4. Метод addNewCat класса Cat должен увеличивать количество котов на 1.

---
+++ src/level_4/Solution4_2/Cat.java
@@ -0,0 +1,13 @@
+package level_4.Solution4_2;
+
+public class Cat {
+    private static int catsCount = 0;
+
+    public static void addNewCat() {
+        Cat.catsCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
