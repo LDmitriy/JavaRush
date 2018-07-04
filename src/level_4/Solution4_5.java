7ca904e40592de00f504fc7863343d3113f474f6
LDmitriy <uks.loboyko@gmail.com>
2018-07-04 17:55:26 +0300

    Кошки во Вселенной Написать код, чтобы правильно считалось количество созданных котов (count) и на экран выдавалось правильное их количество.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Нельзя изменять строку отвечающую за вывод в консоль.
    3. Класс Cat должен содержать только одну переменную count.
    4. Переменная count класса Cat должна быть статической, иметь модификатор доступа public, тип int и проинициализирована нулем.
    5. В методе main должно быть только две проинициализированные переменные типа Cat.
    6. Переменная count должна содержать актуальное количество созданных объектов-котов.

---
+++ src/level_4/Solution4_5.java
@@ -0,0 +1,17 @@
+package level_4;
+
+public class Solution4_5 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat.count++;
+
+        Cat cat2 = new Cat();
+        Cat.count++;
+
+        System.out.println("Cats count is " + Cat.count);
+    }
+
+    public static class Cat {
+        public static int count = 0;
+    }
+}
