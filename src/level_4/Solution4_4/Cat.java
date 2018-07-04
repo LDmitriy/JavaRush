a04e7cede678cd6712838a8e8a3e400fcb8b9cb7
LDmitriy <uks.loboyko@gmail.com>
2018-07-04 17:49:02 +0300

    Программа учета имен Допишите код метода setName, чтобы с его помощью установить значение private String fullName равное значению локальной переменной String fullName.
    Требования:
    1. Класс Cat должен содержать только одну переменную fullName.
    2. Переменная fullName должна иметь модификатор доступа private и тип String.
    3. Класс Cat должен содержать только два метода setName и main.
    4. Метод setName должен иметь локальную переменную fullName.
    5. Метод setName класса Cat должен устанавливать значение глобальной переменной private String fullName равным локальной переменной fullName.

---
+++ src/level_4/Solution4_4/Cat.java
@@ -0,0 +1,15 @@
+package level_4.Solution4_4;
+
+public class Cat {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+
+        this.fullName = fullName;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
