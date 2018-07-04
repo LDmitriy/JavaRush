e311658add0226181020148e2afa2e063495e3ff
LDmitriy <uks.loboyko@gmail.com>
2018-07-04 18:53:42 +0300

    Ближайшее к 10 Напишите метод closeToTen. Метод должен выводить на экран ближайшее к 10 из двух чисел, записанных в аргументах метода. Например, среди чисел 8 и 11 ближайшее к десяти 11. Если оба числа на равной длине к 10, то вывести на экран любое из них.
    Подсказка:
    используйте метод public static int abs(int a), который возвращает абсолютную величину числа.
    
    Требования:
    1. Программа должна выводить числа на экран.
    2. Метод main не должен вызывать System.out.println() или System.out.print().
    3. Метод main должен вызывать метод closeToTen.
    4. Метод closeToTen должен вызывать метод abs.
    5. Метод closeToTen должен выводить число на экран согласно заданию.

---
+++ src/level_4/Solution4_7.java
@@ -0,0 +1,24 @@
+package level_4;
+
+public class Solution4_7 {
+    public static void main(String[] args) {
+        closeToTen(8, 11);
+        closeToTen(7, 14);
+    }
+
+    public static void closeToTen(int a, int b) {
+        if (abs(10-a)<abs(10-b)){
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+
+    public static int abs(int a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+}
