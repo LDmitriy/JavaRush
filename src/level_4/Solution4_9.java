f913536925aea3e917ce1526e110f6275bdf2cbe
LDmitriy <uks.loboyko@gmail.com>
2018-07-04 19:18:53 +0300

    Времена года на Терре Напишите метод checkSeason. По номеру месяца, метод должен определить время года (зима, весна, лето, осень) и вывести на экран.
    Пример для номера месяца 2:
    зима
    
    Пример для номера месяца 5:
    весна
    
    Требования:
    1. Программа должна выводить текст на экран.
    2. Метод main не должен вызывать функцию System.out.println или System.out.print.
    3. Метод main должен вызывать метод checkSeason.
    4. Метод checkSeason должен быть static void, и иметь только один параметр int.
    5. Метод checkSeason должен выводить текст на экран согласно заданию.

---
+++ src/level_4/Solution4_9.java
@@ -0,0 +1,35 @@
+package level_4;
+
+public class Solution4_9 {
+    public static void main(String[] args) {
+        checkSeason(12);
+        checkSeason(4);
+        checkSeason(7);
+        checkSeason(10);
+    }
+
+    public static void checkSeason(int month) {
+        switch (month){
+            case 2:
+            case 12:
+            case 1:
+                System.out.println("зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("осень");
+                break;
+        }
+    }
+}
