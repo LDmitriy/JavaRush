797a86755926c61bceea394e45022b4b407e1003
LDmitriy <uks.loboyko@gmail.com>
2018-07-04 17:46:39 +0300

    Задаем количество котов Напишите метод setCatsCount. Метод должен устанавливать количество котов (catsCount).
    Требования:
    1. Класс Cat должен содержать только одну переменную catsCount.
    2. Переменная catsCount должна быть статической, иметь модификатор доступа private, тип int и проинициализирована нулем.
    3. Класс Cat должен содержать два метода setCatsCount и main.
    4. Метод setCatsCount класса Cat должен устанавливать значение переменной catsCount равным переданному параметру.

---
+++ src/level_4/Solution4_3/Cat.java
@@ -0,0 +1,13 @@
+package level_4.Solution4_3;
+
+public class Cat {
+    private static int catsCount = 0;
+
+    public static void setCatsCount(int catsCount) {
+        Cat.catsCount = catsCount;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
