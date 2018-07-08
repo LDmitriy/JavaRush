304cc156079baaa44ec48e0ac0198fa048e68e90
LDmitriy <uks.loboyko@gmail.com>
2018-07-09 00:53:18 +0300

    Максимум четырех чисел Ввести с клавиатуры четыре числа, и вывести максимальное из них. Если числа равны между собой, необходимо вывести любое.
    Требования:
    1. Программа должна считывать числа c клавиатуры.
    2. Программа должна выводить число на экран.
    3. Программа должна выводить на экран максимальное из четырёх чисел.
    4. Если максимальных чисел несколько, необходимо вывести любое из них.

---
+++ src/level_4/Solution4_17.java
@@ -0,0 +1,36 @@
+package level_4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution4_17 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String x0 = reader.readLine();
+        int a0 = Integer.parseInt(x0);
+
+        String x1 = reader.readLine();
+        int a1 = Integer.parseInt(x1);
+
+        String x2 = reader.readLine();
+        int a2 = Integer.parseInt(x2);
+
+        String x3 = reader.readLine();
+        int a3 = Integer.parseInt(x3);
+
+        min(a0,a1,a2,a3);
+    }
+
+    public static void min (int a,int b,int c, int d){
+        int[] m = {a,b,c,d};
+        for(int i=0; i<3; i++){
+            if (m[i]>m[i+1]){
+                int x = m[i];
+                m[i]=m[i+1];
+                m[i+1]=x;
+            }
+        }
+        System.out.println(m[3]);
+    }
+}
