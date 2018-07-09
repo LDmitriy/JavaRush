29a5bb1f8519f97cc26633b37c7ea3307b4898b8
LDmitriy <uks.loboyko@gmail.com>
2018-07-10 00:29:11 +0300

    Настя или Настя? Ввести с клавиатуры два имени, и если имена одинаковые вывести сообщение "Имена идентичны". Если имена разные, но их длины равны - вывести сообщение - "Длины имен равны". Если имена и длины имен разные - ничего не выводить.
    Требования:
    1. Программа должна считывать две строки c клавиатуры.
    2. Программа должна содержать System.out.println() или System.out.print()
    3. Если имена одинаковые вывести сообщение "Имена идентичны"
    4. Если имена разные, но их длины равны , вывести сообщение "Длины имен равны"
    5. Если имена и длины имен разные - ничего не выводить.

---
+++ src/level_4/Solution4_19.java
@@ -0,0 +1,21 @@
+package level_4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution4_19 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name1 = reader.readLine();
+        String name2  = reader.readLine();
+
+        if (name1.equals(name2)){
+            System.out.println("Имена идентичны");
+        } else{
+            if (name1.length()==name2.length()) System.out.println("Длины имен равны");
+        }
+
+
+    }
+}
