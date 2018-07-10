6c37a3588a5bc9a8514ada99b152ee3d549b8355
LDmitriy <uks.loboyko@gmail.com>
2018-07-10 22:35:35 +0300

    От 1 до 10 Вывести на экран числа от 1 до 10 используя цикл while. Каждое значение с новой строки.
    Требования:
    1. Программа не должна считывать числа c клавиатуры.
    2. Программа должна выводить числа на экран.
    3. Каждое значение должно быть выведено с новой строки.
    4. Программа должна выводить числа от 1 до 10.
    5. В программе должен использоваться цикл while.

---
+++ src/level_4/Solution4_28.java
@@ -0,0 +1,8 @@
+package level_4;
+
+public class Solution4_28 {
+    public static void main(String[] args) throws Exception {
+        int i = 1;
+        while (i<11) System.out.println(i++);
+    }
+}
