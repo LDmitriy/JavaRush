b928d4c007e49e886e487ff77ec8006c2615b50f
LDmitriy <uks.loboyko@gmail.com>
2018-07-10 23:33:41 +0300

    Гадание на долларовый счет Вывести на экран квадрат из 10х10 букв S используя цикл while. Буквы в каждой строке не разделять.
    Пример вывода на экран:
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    SSSSSSSSSS
    
    Требования:
    1. Программа не должна считывать текст c клавиатуры.
    2. Программа должна выводить текст на экран.
    3. Программа должна выводить квадрат из 10х10 букв S.
    4. В программе должен использоваться цикл while.

---
+++ src/level_4/Solution4_31.java
@@ -0,0 +1,17 @@
+package level_4;
+
+public class Solution4_31 {
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+
+        while (i<10){
+            int j = 0;
+            while (j<10){
+                System.out.print("S");
+                j++;
+            }
+            System.out.println("");
+            i++;
+        }
+    }
+}
