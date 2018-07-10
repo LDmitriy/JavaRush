d89c190a46f5509c653720a049b51242a35b7ced
LDmitriy <uks.loboyko@gmail.com>
2018-07-10 23:40:46 +0300

    Таблица умножения Вывести на экран таблицу умножения 10х10 используя цикл while. Числа разделить пробелом.
    Пример вывода на экран:
    1 2 3 4 5 6 7 8 9 10
    2 4 6 8 10 12 14 16 18 20
    3 6 9 12 15 18 21 24 27 30
    4 8 12 16 20 24 28 32 36 40
    5 10 15 20 25 30 35 40 45 50
    6 12 18 24 30 36 42 48 54 60
    7 14 21 28 35 42 49 56 63 70
    8 16 24 32 40 48 56 64 72 80
    9 18 27 36 45 54 63 72 81 90
    10 20 30 40 50 60 70 80 90 100
    
    Требования:
    1. Программа не должна считывать текст c клавиатуры.
    2. Программа должна выводить текст на экран.
    3. Программа должна выводить таблицу умножения 10х10.
    4. В программе должен использоваться цикл while.

---
+++ src/level_4/Solution4_32.java
@@ -0,0 +1,17 @@
+package level_4;
+
+public class Solution4_32 {
+    public static void main(String[] args) throws Exception {
+        int i = 1;
+        while (i<11){
+            int k = 1;
+            while (k<11){
+                System.out.print(i*k+" ");
+                k++;
+            }
+            System.out.println("");
+            i++;
+        }
+
+    }
+}
