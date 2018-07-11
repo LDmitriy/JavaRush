c0458fa250c44d2e1ed95ee45b40ccd320a357be
LDmitriy <uks.loboyko@gmail.com>
2018-07-11 10:16:05 +0300

    Достойная оплата труда Используя цикл вывести на экран сто раз надпись: "Я никогда не буду работать за копейки. Амиго" Каждое значение вывести с новой строки.
    Требования:
    1. Программа не должна считывать текст c клавиатуры.
    2. Программа должна выводить текст на экран.
    3. Программа должна выводить 100 раз текст указанный в задании.
    4. В программе должен использоваться цикл for или цикл while.

---
+++ src/level_4/Solution4_34.java
@@ -0,0 +1,7 @@
+package level_4;
+
+public class Solution4_34 {
+    public static void main(String[] args) {
+        for (int i=0;i<100;i++) System.out.println("Я никогда не буду работать за копейки. Амиго");
+    }
+}
