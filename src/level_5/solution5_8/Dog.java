854cee554d4c4abeb193ded907da7dbfd87e3476
LDmitriy <uks.loboyko@gmail.com>
2018-07-14 00:43:35 +0300

    Создать класс Dog Создать класс Dog (собака) с тремя инициализаторами: - Имя - Имя, рост - Имя, рост, цвет
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. У класса Dog должна быть переменная name с типом String.
    3. У класса Dog должна быть переменная height с типом int.
    4. У класса Dog должна быть переменная color с типом String.
    5. У класса должен быть метод initialize, принимающий в качестве параметра имя и инициализирующий соответствующую переменную класса.
    6. У класса должен быть метод initialize, принимающий в качестве параметров имя, рост и инициализирующий соответствующие переменные класса.
    7. У класса должен быть метод initialize, принимающий в качестве параметров имя, рост, цвет и инициализирующий соответствующие переменные класса.

---
+++ src/level_5/solution5_8/Dog.java
@@ -0,0 +1,26 @@
+package level_5.solution5_8;
+
+public class Dog {
+    String name;
+    int height;
+    String color;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
