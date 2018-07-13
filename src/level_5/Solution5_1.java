763850119e28b0a4a43a5c0467624f5df468a73f
LDmitriy <uks.loboyko@gmail.com>
2018-07-13 12:13:34 +0300

    Трикотаж Пару задач назад студенты секретного центра JavaRush создавали класс Cat. Теперь пришла пора реализовать котов во плоти, разумеется по образу и подобию класса Cat, а точнее - основываясь на нём, как на шаблоне. Их - котов - должно быть трое. Наполните этих троих жизнью, то есть, конкретными данными.
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. Нужно создать три объекта типа Cat.
    3. Класс Cat нельзя изменять.
    4. Программа не должна выводить данные на экран.

--- src/level_5/Solution5_1.java
+++ src/level_5/Solution5_1.java
@@ -1,4 +1,23 @@
 package level_5;
 
 public class Solution5_1 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Vaska",5,4,10);
+        Cat cat2 = new Cat("VtoroyVaska",1,2,2);
+        Cat cat3 = new Cat("Mashka",2,2,3);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
 }
