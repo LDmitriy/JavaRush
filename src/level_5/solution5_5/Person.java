b379c62a0814b7b97d4b74b1b6efb8af54d51c20
LDmitriy <uks.loboyko@gmail.com>
2018-07-13 18:05:25 +0300

    Человечки Создать class Person. У человека должно быть имя String name, возраст int age, адрес String address, пол char sex.
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. У класса Person должна быть переменная name с типом String.
    3. У класса Person должна быть переменная age с типом int.
    4. У класса Person должна быть переменная address с типом String.
    5. У класса Person должна быть переменная sex с типом char.
    6. Всего в классе должно быть 4 переменных.

---
+++ src/level_5/solution5_5/Person.java
@@ -0,0 +1,12 @@
+package level_5.solution5_5;
+
+public class Person {
+    String name;
+    int age;
+    String address;
+    char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
