01ec9c29fc1b21994806739824e7815c256da9ea
LDmitriy <uks.loboyko@gmail.com>
2018-07-13 17:34:03 +0300

    Геттеры и сеттеры для класса Dog Создать class Dog. У собаки должна быть кличка String name и возраст int age. Создайте геттеры и сеттеры для всех переменных класса Dog.
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. У класса Dog должна быть переменная name с типом String.
    3. У класса Dog должна быть переменная age с типом int.
    4. У класса должен сеттер для переменной name.
    5. У класса должен геттер для переменной name.
    6. У класса должен сеттер для переменной age.
    7. У класса должен геттер для переменной age.

---
+++ src/level_5/solution5_4/Dog.java
@@ -0,0 +1,26 @@
+package level_5.solution5_4;
+
+public class Dog {
+    String name;
+    int age;
+
+    void setName (String name){
+        this.name=name;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    void setAge (int age){
+        this.age=age;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
