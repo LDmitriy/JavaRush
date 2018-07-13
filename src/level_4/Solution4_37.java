44eaa0b99e60e556f0611baa742b3a0fac0c4b1a
LDmitriy <uks.loboyko@gmail.com>
2018-07-13 09:56:07 +0300

    Рисуем линии Используя цикл for вывести на экран: - горизонтальную линию из 10 восьмёрок - вертикальную линию из 10 восьмёрок.
    Требования:
    1. Программа не должна считывать числа c клавиатуры.
    2. Программа должна выводить числа на экран.
    3. Программа должна выводить горизонтальную линию из 10 восьмёрок.
    4. Программа должна выводить вертикальную линию из 10 восьмёрок.
    5. В программе должен использоваться цикл for.

---
+++ src/level_4/Solution4_37.java
@@ -0,0 +1,12 @@
+package level_4;
+
+public class Solution4_37 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("8");
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("8");
+        }
+    }
+}
