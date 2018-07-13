19567bcddfa95720757e5a0113609e694e9b8afc
LDmitriy <uks.loboyko@gmail.com>
2018-07-13 09:49:11 +0300

    Треугольник из восьмерок Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
    Пример вывода на экран:
    8
    88
    888
    8888
    88888
    888888
    8888888
    88888888
    888888888
    8888888888
    
    Требования:
    1. Программа не должна считывать числа c клавиатуры.
    2. Программа должна выводить числа на экран.
    3. Программа должна выводить прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
    4. В программе должен использоваться цикл for.

---
+++ src/level_4/Solution4_36.java
@@ -0,0 +1,12 @@
+package level_4;
+
+public class Solution4_36 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0;j<=i;j++){
+                System.out.print("8");
+            }
+            System.out.println("");
+        }
+    }
+}
