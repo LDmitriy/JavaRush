57e7a321a25199abb3ce14c1c06fb9f21157073a
LDmitriy <uks.loboyko@gmail.com>
2018-07-14 11:45:07 +0300

    Создать класс Friend Создать класс Friend (друг) с тремя инициализаторами (тремя методами initialize): - Имя - Имя, возраст - Имя, возраст, пол
    Примечание:
    Имя(String), возраст(int), пол(char).
    
    Требования:
    1. У класса Friend должна быть переменная name с типом String.
    2. У класса Friend должна быть переменная age с типом int.
    3. У класса Friend должна быть переменная sex с типом char.
    4. У класса должен быть метод initialize, принимающий в качестве параметра имя и инициализирующий соответствующую переменную класса.
    5. У класса должен быть метод initialize, принимающий в качестве параметров имя, возраст и инициализирующий соответствующие переменные класса.
    6. У класса должен быть метод initialize, принимающий в качестве параметров имя, возраст, пол и инициализирующий соответствующие переменные класса.

---
+++ src/level_5/solution5_10/Friend.java
@@ -0,0 +1,26 @@
+package level_5.solution5_10;
+
+public class Friend {
+    String name;
+    int age;
+    char sex;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
