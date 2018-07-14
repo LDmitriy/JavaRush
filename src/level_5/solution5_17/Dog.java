ddc7f745ed34969cb8decdb67fc55bf3cb04b7f9
LDmitriy <uks.loboyko@gmail.com>
2018-07-14 16:58:55 +0300

    Регистрируем собачек Создать класс Dog (собака) с тремя конструкторами: - Имя - Имя, рост - Имя, рост, цвет
    Требования:
    1. У класса Dog должна быть переменная name с типом String.
    2. У класса Dog должна быть переменная height с типом int.
    3. У класса Dog должна быть переменная color с типом String.
    4. У класса должен быть конструктор, принимающий в качестве параметра имя и инициализирующий соответствующую переменную класса.
    5. У класса должен быть конструктор, принимающий в качестве параметров имя, рост и инициализирующий соответствующие переменные класса.
    6. У класса должен быть конструктор, принимающий в качестве параметров имя, рост, цвет и инициализирующий соответствующие переменные класса.

---
+++ src/level_5/solution5_17/Dog.java
@@ -0,0 +1,26 @@
+package level_5.solution5_17;
+
+public class Dog {
+    String name;
+    int height;
+    String color;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public Dog(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
