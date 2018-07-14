9fb35968399794110dee1f94b52aded871756d9b
LDmitriy <uks.loboyko@gmail.com>
2018-07-14 23:50:59 +0300

    Шеф, что-то не пашет Задача: Программа считывает два числа с клавиатуры и выводит их сумму на экран.
    Требования:
    1. Программа должна считывать числа с клавиатуры.
    2. В классе должен быть только один метод main.
    3. Программа должна выводить строку, которая начинается на "Sum = ".
    4. Программа должна выводить строку, которая заканчивается суммой введенных чисел.

---
+++ src/level_5/Solution5_25.java
@@ -0,0 +1,15 @@
+package level_5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution5_25 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
