adaf6101965de228cdd77196a8732bd14bf3d1a9
LDmitriy <uks.loboyko@gmail.com>
2018-07-17 22:03:04 +0300

    Классовый счетчик Создать статическую переменную int catCount в классе Cat. Создай конструктор [public Cat()], в котором увеличивай данную переменную на 1.
    Требования:
    1. Добавь в класс Cat конструктор.
    2. Конструктор должен быть public.
    3. Добавь в класс Cat поле catCount.
    4. Поле catCount должно быть типа int.
    5. Поле catCount должно быть статическим.
    6. Конструктор класса должен на 1 увеличивать значение переменной catCount.

---
+++ src/level_6/solution6_6/Cat.java
@@ -0,0 +1,13 @@
+package level_6.solution6_6;
+
+public class Cat {
+    static int catCount;
+
+    public Cat(){
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
