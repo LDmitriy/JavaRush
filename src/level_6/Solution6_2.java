3a13b003f1975e069c8f2e6fe3363d35872d23ca
LDmitriy <uks.loboyko@gmail.com>
2018-07-17 18:54:00 +0300

    По 50 000 объектов Cat и Dog Создать в цикле по 50 000 объектов Cat и Dog. (Java-машина должна начать уничтожать неиспользуемые, и метод finalize хоть раз да вызовется).
    Требования:
    1. В классе Cat должен быть void метод finalize.
    2. В классе Dog должен быть void метод finalize.
    3. Метод finalize класса Cat должен выводить на экран "Cat was destroyed".
    4. Метод finalize класса Dog должен выводить на экран "Dog was destroyed".
    5. Метод main должен создавать 50000 объектов типа Cat и 50000 объектов типа Dog.

---
+++ src/level_6/Solution6_2.java
@@ -0,0 +1,26 @@
+package level_6;
+
+public class Solution6_2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <50000 ; i++) {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+        }
+    }
+}
+
+class Cat {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Cat was destroyed");
+    }
+}
+
+class Dog {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Dog was destroyed");
+    }
+}
