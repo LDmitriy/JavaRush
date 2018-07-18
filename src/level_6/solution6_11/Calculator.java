318887c2218d11c249e026b9bf5f9a1712a299e6
LDmitriy <uks.loboyko@gmail.com>
2018-07-18 23:09:43 +0300

    Калькулятор Сделать класс Calculator, у которого будут 5 статических методов: int plus(int a, int b) - возвращает сумму чисел a и b int minus(int a, int b) - возвращает разницу чисел a и b int multiply(int a, int b) - возвращает результат умножения числа a на число b double division(int a, int b) - возвращает результат деления числа a на число b double percent(int a, int b) - возвращает процент b из числа a
    Требования:
    1. Метод plus должен возвращать сумму чисел a и b.
    2. Метод minus должен возвращать разницу чисел a и b.
    3. Метод multiply должен возвращать результат умножения числа a на число b.
    4. Метод division должен возвращать результат деления числа a на число b.
    5. Метод percent должен возвращать процент b из числа a.

---
+++ src/level_6/solution6_11/Calculator.java
@@ -0,0 +1,27 @@
+package level_6.solution6_11;
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static double percent(int a, int b) {
+        return (double) a * b / 100;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
