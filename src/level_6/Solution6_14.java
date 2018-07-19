7c5a067d21b2857822e77c7454c32b037fac7e9f
LDmitriy <uks.loboyko@gmail.com>
2018-07-19 16:31:53 +0300

    Три статические переменных name Добавь 3 public статических переменных: String Solution.name, String Cat.name, String Dog.name.
    Требования:
    1. В класс Solution добавь public статическую переменную name типа String.
    2. В класс Cat добавь public статическую переменную name типа String.
    3. В класс Dog добавь public статическую переменную name типа String.
    4. В каждом классе должна быть своя переменная name.

---
+++ src/level_6/Solution6_14.java
@@ -0,0 +1,17 @@
+package level_6;
+
+public class Solution6_14 {
+    public static String name;
+
+    public static class Cat {
+        public static String name;
+    }
+
+    public static class Dog {
+        public static String name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
