9586f9b0c3e4a5627ebe756ab005b86690f74d8d
LDmitriy <uks.loboyko@gmail.com>
2018-07-19 16:14:35 +0300

    Феншуй и статики Переставь один модификатор static, чтобы пример скомпилировался.
    Требования:
    1. Метод main менять нельзя.
    2. Добавь модификатор static в нужное место.
    3. Убери лишний модификатор static.
    4. В программе должно быть только 2 модификатора static.

---
+++ src/level_6/Solution6_13.java
@@ -0,0 +1,11 @@
+package level_6;
+
+public class Solution6_13 {
+    public static int A = 5;
+    public int B = 2;
+    public int C = A * B;
+
+    public static void main(String[] args) {
+        A = 15;
+    }
+}
