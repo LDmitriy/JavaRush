f1842c79b885b7ca558b441315d5f101dfad0e66
LDmitriy <uks.loboyko@gmail.com>
2018-07-19 17:38:42 +0300

    KissMyShinyMetalAss Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
    Требования:
    1. Создай класс KissMyShinyMetalAss внутри класса Solution.
    2. Класс KissMyShinyMetalAss должен быть статическим.
    3. Класс KissMyShinyMetalAss должен быть public.
    4. В методе main создай объект класса KissMyShinyMetalAss.
    5. В методе main выведи объект класса KissMyShinyMetalAss на экран.

---
+++ src/level_6/Solution6_16.java
@@ -0,0 +1,12 @@
+package level_6;
+
+public class Solution6_16 {
+    public static class KissMyShinyMetalAss {
+
+    }
+
+    public static void main(String[] args) {
+        KissMyShinyMetalAss ass = new KissMyShinyMetalAss();
+        System.out.println(ass);
+    }
+}
