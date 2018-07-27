2140afe8da95ec1ab96fea4b2a9359c9f95bb82c
LDmitriy <uks.loboyko@gmail.com>
2018-07-27 19:36:20 +0300

    Что за список такой? 1. Создай список строк. 2. Добавь в него 5 различных строк. 3. Выведи его размер на экран. 4. Используя цикл выведи его содержимое на экран, каждое значение с новой строки.
    Требования:
    1. Программа не должна ничего считывать с клавиатуры.
    2. Объяви переменную типа ArrayList (список строк) и сразу проинициализируй ee.
    3. Программа должна добавить 5 любых строк в список.
    4. Программа должна вывести размер списка на экран.
    5. Программа должна вывести содержимое списка на экран, каждое значение с новой строки.

---
+++ src/level_7/Solution7_6.java
@@ -0,0 +1,18 @@
+package level_7;
+
+import java.util.ArrayList;
+
+public class Solution7_6 {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("safgh");
+        list.add("hgjd");
+        list.add("undg");
+        list.add("hydrte");
+        list.add("vergjf");
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
