31efb4d3bc6f9d384c8f9d34b6392a964d933f77
LDmitriy <uks.loboyko@gmail.com>
2018-07-30 21:21:03 +0300

    Обратный отсчёт Написать программу, которая ведёт обратный отсчёт с 30 до 0, а в конце выводит на экран текст "Бум!". Программа должна уменьшать число 10 раз в секунду. Для того чтобы вставить в программу задержку, воспользуйся функцией: Thread.sleep(100); //задержка на одну десятую секунды.
    Пример:
    30
    29
    ...
    1
    0
    Бум!
    
    Требования:
    1. Программа не должна считывать данные с клавиатуры.
    2. Программа должна выводить обратный отсчет от 30 до 0. Каждую цифру на новой строке.
    3. Программа должна выводить "Бум!" после отсчета.
    4. Программа должна использовать метод "Thread.sleep(100);" 31 раз.

---
+++ src/level_7/Solution7_22.java
@@ -0,0 +1,13 @@
+package level_7;
+
+public class Solution7_22 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+
+            Thread.sleep(100);
+        }
+
+        System.out.println("Бум!");
+    }
+}
