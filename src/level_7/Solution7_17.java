37c19115cfc68dec5645021e488b547f0e44efa4
LDmitriy <uks.loboyko@gmail.com>
2018-07-30 03:50:13 +0300

    Проверка на упорядоченность 1. Введи с клавиатуры 10 слов в список строк. 2. Определить, является ли список упорядоченным по возрастанию длины строки. 3. В случае отрицательного ответа вывести на экран индекс первого элемента, нарушающего такую упорядоченность.
    Требования:
    1. Объяви переменную типа список строк и сразу проинициализируй ee.
    2. Считай 10 строк с клавиатуры и добавь их в список.
    3. Если список упорядочен по возрастанию длины строки, то ничего выводить не нужно.
    4. Если список не упорядочен по возрастанию длины строки, то нужно вывести на экран индекс первого элемента, нарушающего такую упорядоченность.

---
+++ src/level_7/Solution7_17.java
@@ -0,0 +1,21 @@
+package level_7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution7_17 {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).length() < list.get(i + 1).length()) {
+                continue;
+            } else System.out.println(i + 1);
+        }
+    }
+}
