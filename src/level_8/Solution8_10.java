31ce5fed09e0d9604728582f604df5da3e2225a4
LDmitriy <uks.loboyko@gmail.com>
2018-07-31 23:59:50 +0300

    Квартет «Методы» Реализовать 4 метода. Каждый из них должен возвращать список, который лучше всего подходит для выполнения данных операций (быстрее всего справится с большим количеством операций). Ничего измерять не нужно.
    Требования:
    1. Программа не должна выводить текст на экран.
    2. Класс Solution должен содержать только 5 методов.
    3. Метод getListForGet должен возвращать список, который лучше всего подходит для операции взятия элемента.
    4. Метод getListForSet должен возвращать список, который лучше всего подходит для установки значения элемента.
    5. Метод getListForAddOrInsert должен возвращать список, который лучше всего подходит для операций добавления или вставки элемента.
    6. Метод getListForRemove должен возвращать список, который лучше всего подходит для операции удаления элемента.

---
+++ src/level_8/Solution8_10.java
@@ -0,0 +1,31 @@
+package level_8;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution8_10 {
+    public static List getListForGet() {
+        List<Object> list = new ArrayList<>();
+        return list;
+    }
+
+    public static List getListForSet() {
+        List<Object> list = new ArrayList<>();
+        return list;
+    }
+
+    public static List getListForAddOrInsert() {
+        List<Object> list = new LinkedList<>();
+        return list;
+    }
+
+    public static List getListForRemove() {
+        List<Object> list = new LinkedList<>();
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
