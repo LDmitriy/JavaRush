0684575ebf9626d0f06afeca26078e55d5b000bc
LDmitriy <uks.loboyko@gmail.com>
2018-08-01 21:25:23 +0300

    Перепись населения Создать словарь (Map<String, String>) занести в него десять записей по принципу "Фамилия" - "Имя". Проверить сколько людей имеют совпадающие с заданным именем или фамилией.
    Требования:
    1. Программа не должна выводить текст на экран.
    2. Программа не должна считывать значения с клавиатуры.
    3. Метод createMap() должен создавать и возвращать словарь HashMap с типом элементов String, String состоящих из 10 записей по принципу «Фамилия» - «Имя».
    4. Метод getCountTheSameFirstName() должен возвращать число людей у которых совпадает имя.
    5. Метод getCountTheSameLastName() должен возвращать число людей у которых совпадает фамилия.

---
+++ src/level_8/Solution8_14.java
@@ -0,0 +1,45 @@
+package level_8;
+
+import java.util.HashMap;
+
+public class Solution8_14 {
+    public static HashMap<String, String> createMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("dhg", "gghj");
+        map.put("dghj", "gghj");
+        map.put("jhsd", "gghj");
+        map.put("shju", "gghj");
+        map.put("uy", "gghj");
+        map.put("fkuio", "gghj");
+        map.put("fjddhg", "gghj");
+        map.put("ftd", "gghj");
+        map.put("fj", "gghj");
+        map.put("rm,", "gghj");
+        return map;
+    }
+
+    public static int getCountTheSameFirstName(HashMap<String, String> map, String name) {
+        int count = 0;
+        for (HashMap.Entry<String, String> s: map.entrySet()){
+            if (s.getValue().equals(name)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int getCountTheSameLastName(HashMap<String, String> map, String lastName) {
+        int count = 0;
+        for (HashMap.Entry<String, String> s: map.entrySet()){
+            if (s.getKey().equals(lastName)){
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
