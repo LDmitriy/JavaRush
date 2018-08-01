6c147c2e156a6d2e5236c745817e7ec1be1af408
LDmitriy <uks.loboyko@gmail.com>
2018-08-01 18:47:36 +0300

    20 слов на букву «Л» Создать множество строк (Set<String>), занести в него 20 слов на букву "Л".
    Требования:
    1. Не изменяй заголовок метода createSet().
    2. Программа не должна выводить текст на экран.
    3. Программа не должна считывать значения с клавиатуры.
    4. Метод createSet() должен создавать и возвращать множество (реализация HashSet).
    5. Множество из метода createSet() должно содержать 20 слов на букву «Л».

---
+++ src/level_8/Solution8_12.java
@@ -0,0 +1,36 @@
+package level_8;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution8_12 {
+    public static Set<String> createSet() {
+        HashSet<String> set = new HashSet<>();
+        set.add("Лом");
+        set.add("Лук");
+        set.add("Люк");
+        set.add("Люстра");
+        set.add("Люди");
+        set.add("Люмпен");
+        set.add("Луч");
+        set.add("Луза");
+        set.add("Лоза");
+        set.add("Лютер");
+        set.add("Локоть");
+        set.add("Ломоть");
+        set.add("Лошадь");
+        set.add("Лопата");
+        set.add("Лорд");
+        set.add("Локон");
+        set.add("Лупа");
+        set.add("Лидер");
+        set.add("Лодка");
+        set.add("Люфт");
+
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
