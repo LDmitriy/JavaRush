f3af849ec04bcc0ead0549a34214f2fb56383018
LDmitriy <uks.loboyko@gmail.com>
2018-08-04 00:09:56 +0300

    Однофамильцы и тёзки 1. Создать словарь Map (<String, String>) и добавить туда 10 человек в виде "Фамилия"-"Имя". 2. Пусть среди этих 10 человек есть люди с одинаковыми именами. 3. Пусть среди этих 10 человек есть люди с одинаковыми фамилиями. 4. Вывести содержимое Map на экран.
    Требования:
    1. Программа должна выводить текст на экран.
    2. Класс Solution должен содержать только три метода.
    3. Метод createPeopleList() должен создавать и возвращать словарь Map с типом элементов String, String. Кроме того, добавлять в словарь 10 человек.
    4. В методе createPeopleList() должны добавляться люди с одинаковыми фамилиями.
    5. В методе createPeopleList() должны добавляться люди с одинаковыми именами.
    6. Метод printPeopleList() должен выводить на экран всех людей из словаря Map. Каждое значение вывести с новой строки. Фамилия и имя разделены пробелом.
    7. Метод main() должен вызывать метод createPeopleList().
    8. Метод main() должен вызывать метод printPeopleList().

---
+++ src/level_8/Solution8_19.java
@@ -0,0 +1,33 @@
+package level_8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution8_19 {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleList();
+        printPeopleList(map);
+    }
+
+    public static Map<String, String> createPeopleList() {
+        Map<String,String> map = new HashMap<>();
+        map.put("q","a");
+        map.put("q","s");
+        map.put("w","d");
+        map.put("e","d");
+        map.put("r","f");
+        map.put("t","g");
+        map.put("y","h");
+        map.put("u","j");
+        map.put("i","k");
+        map.put("o","l");
+
+        return map;
+    }
+
+    public static void printPeopleList(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
